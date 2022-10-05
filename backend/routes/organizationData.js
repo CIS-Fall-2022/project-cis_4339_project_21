e2c131749d27c8aab28771265b8b13eacb52bf1a
oyesquared <oyewosamuel99@gmail.com>
2022-10-04 21:06:58 -0500

    Adding an organization file so that it can be used later

---
+++ backend/routes/organizationData.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+//allow using a .env file
+require("dotenv").config();  
+
+const ORGANIZATION = process.env.ORGANIZATION ;
+
+//GET organization name
+router.get("/organization", (req, res, next) => { 
+    res.json({ "name": ORGANIZATION });
+});
+
+module.exports = router;
