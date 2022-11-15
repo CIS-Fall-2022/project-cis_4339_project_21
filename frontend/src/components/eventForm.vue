ee795de746d3ae80f2f219fbaf97dea33295a939
Bryan Chavarria <77865189+BryanChava@users.noreply.github.com>
2022-11-14 19:48:49 -0600

    added event org/type

--- frontend/src/components/eventForm.vue
+++ frontend/src/components/eventForm.vue
@@ -14,7 +14,11 @@
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Event Name</span>
-              <span style="color:#ff0000">*</span>
+            <span style="color:#ff0000">*</span>
+            <input
+              type="hidden"
+              v-model="event.organization"
+            />
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
