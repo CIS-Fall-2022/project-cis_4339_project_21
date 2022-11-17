f2cacce39a3c6ae321e67b1a81592257012b5b81
jdfitzge <42559037+jdfitzge@users.noreply.github.com>
2022-11-16 19:56:32 -0600

    commit

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -1,37 +1,42 @@
 <template>
   <main>
     <div>
-      <h1 class="font-bold text-4xl text-Black-700 tracking-widest text-center mt-10">Welcome</h1>
+  <!-- hello message -->
+      <h1 class="font-bold text-5xl text-red-700 tracking-widest text-center mt-12">Greetings!</h1>
     </div>
-
+    
+  <!-- creating bar chart declarations -->
     <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
+    :chart-data="chart_Data"
+    :chart-id="chart_Id"
+    :chart-options="chart_Options"
+    :dataset-id-key="dataset_Id_Key"
     :plugins="plugins"
-    :css-classes="cssClasses"
+    :css-classes="css_Classes"
     :styles="styles"
     :width="width"
     :height="height"
    />
-    <div class="p-10">
-      <div class="ml-10">
-        <h2 class="text-2xl font-bold text-center">Event count by clients </h2>
-        <h3 class="text-center">Clients signed up for each event for the last 2 months</h3>
+
+
+<!-- setting up text visual format for text  -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+      <div class="ml-12">
+        <h2 class="italic">Count of Events by Client</h2>
+        <h3 class="italic"># of Clients Signed For Each Event</h3>
       </div>
       <div class="flex flex-col col-span-2">
         <table class="min-w-full shadow-md rounded">
           <thead class="bg-gray-50 text-xl">
             <tr>
-              <th class="p-10 text-center">Event Name</th>
-              <th class="p-10 text-center"># Client Signed up</th>
+              <th class="p-2 text-left">The Event Name</th>
+              <th class="p-2 text-left"># of Clients Signed Up</th>
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
             <tr v-for="event in queryData">
-            <td class="p-2 text-center">{{ event.eventName }}</td>
-            <td class="p-2 text-center">{{ event.number_of_clients_signed_up }}</td>
+            <td class="p-2 text-left">{{ event.eventName }}</td>
+            <td class="p-2 text-left">{{ event.number_of_clients_signed_up }}</td>
       </tr>
     </tbody>
   </table>
@@ -50,23 +55,23 @@
   name: 'BarChart',
   components: { Bar },
   props: {
-    chartId: {
+    chart_Id: {
       type: String,
       default: 'bar-chart'
     },
-    datasetIdKey: {
+    dataset_Id_Key: {
       type: String,
       default: 'label'
     },
     width: {
       type: Number,
-      default: 200
+      default: 150
     },
     height: {
       type: Number,
-      default: 50
+      default: 60
     },
-    cssClasses: {
+    css_Classes: {
       default: '',
       type: String
     },
@@ -82,27 +87,28 @@
   data() {
     return {
       queryData: [],
-      chartData: {
+      chart_Data: {
         labels: [],
-        datasets: [ { data: [], label: 'Show/Hide' ,backgroundColor: '#C8102E',} ]
+        datasets: [ { data: [], label: 'Show or Hide Here' ,backgroundColor: '#FF0000',} ]
       },
-      chartOptions: {
+      chart_Options: {
         responsive: true,
         scales: {
         y: {
             min: 1,
-            max: 10,
+            max: 20,
         }
     }
       }
     };
   },    
+  // using axios get methods to retrieve responses
   mounted() {
     let apiURL = import.meta.env.VITE_ROOT_API + `/eventdata/clientsByEvents`;
     axios.get(apiURL).then((resp) => {
       this.queryData = resp.data;
-      this.chartData.labels = resp.data.map(value => value.eventName);
-      this.chartData.datasets[0].data = resp.data.map(value => value.number_of_clients_signed_up);
+      this.chart_Data.labels = resp.data.map(value => value.eventName);
+      this.chart_Data.datasets[0].data = resp.data.map(value => value.number_of_clients_signed_up);
     });
     window.scrollTo(0, 0);
   },
